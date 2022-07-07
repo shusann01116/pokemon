d8067d5ce137518274aa660965c33d75eced5777
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2022-07-07 06:18:54 +0000

    feat: Add pip install functionality post install

--- .devcontainer/post_install.sh
+++ .devcontainer/post_install.sh
@@ -3,4 +3,5 @@
 set -xo pipefail
 
 curl -sfL https://releases.dagger.io/dagger/install.sh | sh
-echo "export PATH=\$PATH:$(pwd)/bin" >> ~/.zshrc
+echo "export PATH=\$PATH:$(pwd)/bin" >>~/.zshrc
+pip install -r $(pwd)/requirements.txt
